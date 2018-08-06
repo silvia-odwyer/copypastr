1e2004ee1e7daa027c7a263a0eb224f12c02ef3b
silvia-odwyer <silviaodwyer119@yahoo.ie>
2018-08-06 15:20:21 +0100

    Emoji searching functionality now working.

--- src/pages/add-emoji/add-emoji.ts
+++ src/pages/add-emoji/add-emoji.ts
@@ -1,28 +1,41 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 
-/**
- * Generated class for the AddEmojiPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @Component({
   selector: 'page-add-emoji',
   templateUrl: 'add-emoji.html',
 })
 export class AddEmojiPage {
+  public emoji_dict = {"happy" : "😀", "grin" : "😁", "lmao" : "😂", "rofl" : "🤣"};
+  public emoji_keys = Object.keys(this.emoji_dict);
 
   constructor(public navCtrl: NavController, public navParams: NavParams,  public view: ViewController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddEmojiPage');
+    // let emoji_dict = this.emoji_dict;
+    // let emoji_keys = this.emoji_keys;
   }
 
   close() {
     this.view.dismiss();
+  }
+
+  resetItems() {
+    this.emoji_keys = Object.keys(this.emoji_dict);
+
+  }
+
+  getEmoji(ev: any) {
+    const val = ev.target.value;
+    this.resetItems();
+    
+    if (val && val.trim() != '') {
+      this.emoji_keys = this.emoji_keys.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
   }
 
 }
