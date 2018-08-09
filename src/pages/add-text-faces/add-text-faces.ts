8ac87b8ce02ae438a28473c9277922dcaaa0f2c8
silvia-odwyer <silviaodwyer119@yahoo.ie>
2018-08-09 22:20:59 +0100

    Added clipboard copying functionality to the Add Text Faces page.

--- src/pages/add-text-faces/add-text-faces.ts
+++ src/pages/add-text-faces/add-text-faces.ts
@@ -1,30 +1,54 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, ViewController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the AddTextFacesPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { IonicPage, NavController, ViewController, NavParams, ToastController } from 'ionic-angular';
+import * as Clipboard from 'clipboard/dist/clipboard.min.js';
 
 @Component({
   selector: 'page-add-text-faces',
   templateUrl: 'add-text-faces.html',
 })
 export class AddTextFacesPage {
-  public text_faces = ["( ͡° ͜ʖ ͡°)", "¯\\_(ツ)_/¯", "( ͡°( ͡° ͜ʖ( ͡° ͜ʖ ͡°)ʖ ͡°) ͡°)", "≧◡≦", "(▰˘◡˘▰)", "(▀̿Ĺ̯▀̿ ̿)", "(ง ͠° ͟ل͜ ͡°)ง","༼ つ ◕_◕ ༽つ", "ಠ_ಠ", "(づ｡ ◕‿‿◕｡) づ ", "[̲̅$̲̅(̲̅5̲̅)̲̅$̲̅]]", "(∩︵∩)"];
-  public sad_text_faces = ["v( ‘.’ )v", "●︿●", "◄.►", "◄︿►", "(◕︵◕)", "≧︵≦", " ͡°︵°"];
-  public cute_text_faces = ["ʕ•ᴥ•ʔ", "(●´ω｀●)", , "(ﾉ◕ヮ◕)ﾉ*:・ﾟ✧ ✧ﾟ・: *ヽ(◕ヮ◕ヽ)", "ʕʘ‿ʘʔ", "(✿◠‿◠)", "(✿◠‿◠) *:・ﾟ✧ ✧"];
-  constructor(public navCtrl: NavController, public navParams: NavParams, public view: ViewController) {
+  public text_faces = ["( ͡° ͜ʖ ͡°)", "¯\\_(ツ)_/¯", "( ͡°( ͡° ͜ʖ( ͡° ͜ʖ ͡°)ʖ ͡°) ͡°)", "≧◡≦", "(▰˘◡˘▰)", "(▀̿Ĺ̯▀̿ ̿)", "(ง ͠° ͟ل͜ ͡°)ง", "༼ つ ◕_◕ ༽つ", "ಠ_ಠ", "(づ｡ ◕‿‿◕｡) づ ", "[̲̅$̲̅(̲̅5̲̅)̲̅$̲̅]]", "(∩︵∩)"];
+  public sad_text_faces = ["(◡︿◡✿)", "(✖╭╮✖)", "(✖︿✖)", "(✖︵✖)", "(✖.✖)", "v( ‘.’ )v", "●︿●", "◄.►", "◄︿►", "(◕︵◕)", "≧︵≦", " ͡°︵°"];
+  public romance_text_faces = ["(• ε •)", "(♥ ε ♥)", "(◕ ε ◕)", "♥‿♥", "(ﾉ◕ヮ◕)ﾉ*:・ﾟ♥ ✧ﾟ♥:ﾟ* ♥ﾟ✧ﾟヽ(◕ヮ◕ヽ)"]
+  public cute_text_faces = ["~,~", "⊂◉‿◉つ", "●ᴥ●", "(◕‿◕✿)", "ʕ•ᴥ•ʔ", "(●´ω｀●)", , "(ﾉ◕ヮ◕)ﾉ*:・ﾟ✧ ✧ﾟ・: *ヽ(◕ヮ◕ヽ)", "ʕʘ‿ʘʔ", "(✿◠‿◠)", "(✿◠‿◠) *:・ﾟ✧ ✧"];
+  public happy_text_faces = ["≖‿≖", "٩(^‿^)۶", "◕‿◕", "✖‿✖"];
+  public angry_text_faces = ["►.◄", "{(>_<)}", "ˇ︿ˇ", ">.<"];
+  
+  public clipboard;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public view: ViewController, private toastCtrl: ToastController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddTextFacesPage');
+
+    this.clipboard = new Clipboard('.text_faces', {
+      target: function (trigger) {
+        return trigger;
+      }
+    }); 
+
+    this.clipboard.on('success', () => this.displayToast("success"));
+
+    this.clipboard.on('error', function(e) {
+      console.error('Action:', e.action);
+      console.error('Trigger:', e.trigger);
+      this.displayToast("error");
+  });
   }
 
   close() {
     this.view.dismiss();
+  }
+
+  displayToast(state) {
+    let state_dict = {"error" : "Error copying the text face. Try again.", "success" : "Copied & added to your favourites"};
+    let toast = this.toastCtrl.create({
+      message: state_dict[state],
+      duration: 1000,
+      position: 'bottom'
+    });
+    toast.present();
   }
 
 }
