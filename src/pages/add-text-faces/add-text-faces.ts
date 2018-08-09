af19c3c4830a86c57addc562c75a3e4dd7830f90
silvia-odwyer <silviaodwyer119@yahoo.ie>
2018-08-09 21:12:06 +0100

    Angular binding of text faces array now works with the template in the HTML.

--- src/pages/add-text-faces/add-text-faces.ts
+++ src/pages/add-text-faces/add-text-faces.ts
@@ -13,7 +13,9 @@ import { IonicPage, NavController, ViewController, NavParams } from 'ionic-angul
   templateUrl: 'add-text-faces.html',
 })
 export class AddTextFacesPage {
-
+  public text_faces = ["( ͡° ͜ʖ ͡°)", "¯\\_(ツ)_/¯", "( ͡°( ͡° ͜ʖ( ͡° ͜ʖ ͡°)ʖ ͡°) ͡°)", "≧◡≦", "(▰˘◡˘▰)", "(▀̿Ĺ̯▀̿ ̿)", "(ง ͠° ͟ل͜ ͡°)ง","༼ つ ◕_◕ ༽つ", "ಠ_ಠ", "(づ｡ ◕‿‿◕｡) づ ", "[̲̅$̲̅(̲̅5̲̅)̲̅$̲̅]]", "(∩︵∩)"];
+  public sad_text_faces = ["v( ‘.’ )v", "●︿●", "◄.►", "◄︿►", "(◕︵◕)", "≧︵≦", " ͡°︵°"];
+  public cute_text_faces = ["ʕ•ᴥ•ʔ", "(●´ω｀●)", , "(ﾉ◕ヮ◕)ﾉ*:・ﾟ✧ ✧ﾟ・: *ヽ(◕ヮ◕ヽ)", "ʕʘ‿ʘʔ", "(✿◠‿◠)", "(✿◠‿◠) *:・ﾟ✧ ✧"];
   constructor(public navCtrl: NavController, public navParams: NavParams, public view: ViewController) {
   }
 
