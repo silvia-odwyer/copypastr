9693581eea398077ceed942656886c46739fe249
silvia-odwyer <silviaodwyer119@yahoo.ie>
2018-08-21 21:56:51 +0100

    New sections in the Emoji Types Page.

--- src/pages/emoji-types/emoji-types.ts
+++ src/pages/emoji-types/emoji-types.ts
@@ -15,8 +15,12 @@ export class EmojiTypesPage {
   public emoji_lib = emoji.lib;
   public emotion_emoji_array = [];
   public ani_nature_emoji_array = [];
+  public activities_emoji_array = [];
+  public objects_emoji_array = [];
+  public favourites = [];
+  public clipboard;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController) {
     this.emoji_type = "emotions";
   }
 
@@ -25,7 +29,9 @@ export class EmojiTypesPage {
 
     this.populateEmojiArray("people", this.emotion_emoji_array);
     this.populateEmojiArray("animals_and_nature", this.ani_nature_emoji_array);
-
+    this.populateEmojiArray("activity", this.activities_emoji_array);
+    this.populateEmojiArray("objects", this.objects_emoji_array);
+    this.initialiseClipboard();
   }
 
   populateEmojiArray(category, emoji_array) {
@@ -38,6 +44,36 @@ export class EmojiTypesPage {
         emoji_array.push(emoji.char);
       }
     }
+  }
+
+  initialiseClipboard() {
+    this.clipboard = new Clipboard('.emoji_c', {
+      target: function (trigger) {
+        return trigger;
+      }
+    });
+
+    this.clipboard.on('error', function (e) {
+      console.error('Action:', e.action);
+      console.error('Trigger:', e.trigger);
+      this.displayToast("error");
+    });
+
+  }
+
+  displayToast(state) {
+    let state_dict = {"error" : "Error copying your emoji. Try again.", "success" : "Emoji copied & added to your favourites"};
+    let toast = this.toastCtrl.create({
+      message: state_dict[state],
+      duration: 1000,
+      position: 'bottom'
+    });
+    toast.present();
+  }
+
+  addToFavourites(item) {
+    this.favourites.push(item);
+    this.displayToast("success");
   }
 
 }
