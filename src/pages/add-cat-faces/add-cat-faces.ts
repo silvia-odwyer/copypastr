da57077754f2e727119a9cb14eddb95a0f1ad812
silvia-odwyer <silviaodwyer119@yahoo.ie>
2018-08-22 19:08:19 +0100

    Cat ascii faces now available.

--- src/pages/add-cat-faces/add-cat-faces.ts
+++ src/pages/add-cat-faces/add-cat-faces.ts
@@ -1,24 +1,21 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the AddCatFacesPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import * as cats from "cat-ascii-faces";
 
 @Component({
   selector: 'page-add-cat-faces',
   templateUrl: 'add-cat-faces.html',
 })
 export class AddCatFacesPage {
+  public cat_ascii_faces = cats.cats; // list of cat ascii faces
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddCatFacesPage');
+    console.log(this.cat_ascii_faces);
+
   }
 
 }
