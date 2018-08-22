540a847e67fb95a4ae4bf94f861021943b6522a9
silvia-odwyer <silviaodwyer119@yahoo.ie>
2018-08-22 16:25:40 +0100

    All Western text face sections now displaying.

--- src/pages/western-text-faces/western-text-faces.ts
+++ src/pages/western-text-faces/western-text-faces.ts
@@ -6,8 +6,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'western-text-faces.html',
 })
 export class WesternTextFacesPage {
+  public western_happy_text_faces = [":‑)", ":)", ":-]", ":]", ":-3", ":3", ":->", ":>", "8-)", "8)", ":-}", ":}", ":o)", ":c)", ":^)", "=]", "=)" , ":‑D", ":D", 	"8‑D", "8D", "x‑D", "xD", "X‑D", "XD", "=D", "=3", "B^D", ":'‑)", ":')"];
+  public western_sad_text_faces = [":‑(", ":(", ":‑c", ":c", ":‑<", ":<", ":‑[", ":[", ":-||", ">:[", ":{", ":@", ">:(", ":'‑(", ":'("];
+  public western_angry_text_faces = [">.<", "v.v"];
+  public western_cute_text_faces = [":‑P", ":P", "X‑P", "XP", "x‑p", "xp", ":‑p", ":p", ":‑Þ", ":Þ", ":‑þ", ":þ", ":‑b", ":b", "d:", "=p", ">:P"];
+  public western_romance_text_faces = [":-*", ":*", ":×"]
 
+  // Set active segment
+  public west_category;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  this.west_category = "western_happy_text_faces";
   }
 
   ionViewDidLoad() {
