be6057863f97c1f1b20513c0497205c3f3756a1f
silvia-odwyer <silviaodwyer119@yahoo.ie>
2018-08-25 19:11:31 +0100

    Trending GIFS can now be fetched from GIPHY.

--- src/pages/trending-gifs/trending-gifs.ts
+++ src/pages/trending-gifs/trending-gifs.ts
@@ -1,17 +1,62 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Http } from '@angular/http/';
+import * as giphy_api_obj from "../../../env/giphy_api_key.json"
+import { ToastController } from 'ionic-angular/components/toast/toast-controller';
 
 @Component({
   selector: 'page-trending-gifs',
   templateUrl: 'trending-gifs.html',
 })
 export class TrendingGifsPage {
+  public posts;
+  public link_array = [];
+  public giphy_api_key = giphy_api_obj["api_key"];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http, public toastCtrl: ToastController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TrendingGifsPage');
+
+    this.getTrendingGIFs();
+  }
+
+  getTrendingGIFs() {
+    this.http.get(`https://api.giphy.com/v1/gifs/trending?api_key=${this.giphy_api_key}&limit=30&rating=g`).map(res => res.json()).subscribe(data => {
+      this.posts = data.data;
+      let link_count = 0;
+      for (let k = 0; link_count < 20; k += 1) {
+        let ranIndex = this.getRandomNumber(0, this.posts.length - 1);
+        let link = this.posts[ranIndex].images.original.url;
+        if (this.link_array.indexOf(link) > -1 ){
+          continue;
+        }
+        else {
+          this.link_array.push(link);
+          link_count += 1;
+        }
+
+      }
+    });
+  }
+
+  getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  addToFavourites(item) {
+
+  }
+
+  displayToast(state) {
+    let state_dict = { "error": "Error copying your emoji. Try again.", "success": "Emoji copied & added to your favourites" };
+    let toast = this.toastCtrl.create({
+      message: state_dict[state],
+      duration: 1000,
+      position: 'bottom'
+    });
+    toast.present();
   }
 
 }
