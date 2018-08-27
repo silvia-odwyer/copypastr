ca18d6739b085f7b467ce91855f086d307c76b5e
silvia-odwyer <silviaodwyer119@yahoo.ie>
2018-08-27 16:12:21 +0100

    Trending GIF page layout changes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,45 +4,48 @@ import { HttpModule } from '@angular/http';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { ProgressBarComponent } from "../components/progress-bar/progress-bar";
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { AddEmojiPage } from '../pages/add-emoji/add-emoji';
 import { AddTextFacesPage } from '../pages/add-text-faces/add-text-faces';
 import { AddCatFacesPage } from '../pages/add-cat-faces/add-cat-faces';
-import { AddNumbersPage } from '../pages/add-numbers/add-numbers';
 import { FavouritesPage } from '../pages/favourites/favourites';
 import { CoolSymbolsPage } from '../pages/cool-symbols/cool-symbols';
 import { GetGifsPage } from '../pages/get-gifs/get-gifs';
-import {StickerTab} from "../pages/sticker-tab/sticker-tab";
-import {EmotionEmojiPage} from "../pages/emotion-emoji/emotion-emoji";
+import { EmotionEmojiPage } from "../pages/emotion-emoji/emotion-emoji";
 import { AnimalsNaturePage } from '../pages/animals-nature/animals-nature';
 import { EmojiTypesPage } from '../pages/emoji-types/emoji-types';
 import { WesternTextFacesPage } from '../pages/western-text-faces/western-text-faces';
-import { EasternAnimatedStickersPage } from '../pages/eastern-animated-stickers/eastern-animated-stickers';
 import { EmojifyPage } from '../pages/emojify/emojify';
 import { ServeGifsPage } from '../pages/serve-gifs/serve-gifs';
-
+import { ChangeDetectorRef } from '@angular/core/src/change_detection/change_detector_ref';
+import { SearchStickersPage } from '../pages/search-stickers/search-stickers';
+import { TrendingGifsPage } from '../pages/trending-gifs/trending-gifs';
+import { TrendingStickersPage } from '../pages/trending-stickers/trending-stickers';
 
 @NgModule({
   declarations: [
     MyApp,
+
     HomePage,
     AddEmojiPage,
     AddTextFacesPage,
     AddCatFacesPage,
-    AddNumbersPage,
     FavouritesPage,
     CoolSymbolsPage,
-    StickerTab,
+    SearchStickersPage,
+    TrendingStickersPage,
     WesternTextFacesPage,
     EmotionEmojiPage,
     AnimalsNaturePage,
+    ProgressBarComponent,
     ServeGifsPage,
     EmojifyPage,
     EmojiTypesPage,
     GetGifsPage,
-    EasternAnimatedStickersPage
+    TrendingGifsPage,
   ],
   imports: [
     BrowserModule,
@@ -56,23 +59,24 @@ import { ServeGifsPage } from '../pages/serve-gifs/serve-gifs';
     AddEmojiPage,
     AddTextFacesPage,
     AddCatFacesPage,
-    StickerTab,
     EmotionEmojiPage,
     EmojifyPage,
     ServeGifsPage,
-    AddNumbersPage,
+    SearchStickersPage,
+    TrendingStickersPage,
     WesternTextFacesPage,
-    EasternAnimatedStickersPage,
     FavouritesPage,
     CoolSymbolsPage,
+    ProgressBarComponent,
     GetGifsPage,
     AnimalsNaturePage,
-    EmojiTypesPage
+    EmojiTypesPage,
+    TrendingGifsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
