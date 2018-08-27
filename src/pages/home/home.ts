7a6a750f715a635c3cded2e0dc3ea58accca820e
silvia-odwyer <silviaodwyer119@yahoo.ie>
2018-08-27 14:32:49 +0100

    Stylised Copy button links and deleted irrelevant pages.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,16 +3,13 @@ import { ModalController, NavController } from 'ionic-angular';
 import { AddEmojiPage } from '../add-emoji/add-emoji';
 import { AddTextFacesPage } from '../add-text-faces/add-text-faces';
 import { AddCatFacesPage } from '../add-cat-faces/add-cat-faces';
-import { AddNumbersPage } from '../add-numbers/add-numbers';
 import { FavouritesPage } from '../favourites/favourites';
 import { CoolSymbolsPage } from '../cool-symbols/cool-symbols';
 import { GetGifsPage } from '../get-gifs/get-gifs';
-import { StickerTab } from '../sticker-tab/sticker-tab';
 import { EmotionEmojiPage } from "../emotion-emoji/emotion-emoji"
 import { AnimalsNaturePage } from "../animals-nature/animals-nature";
 import { EmojiTypesPage } from '../emoji-types/emoji-types';
 import { WesternTextFacesPage } from '../western-text-faces/western-text-faces';
-import { EasternAnimatedStickersPage } from '../eastern-animated-stickers/eastern-animated-stickers';
 import { EmojifyPage } from '../emojify/emojify';
 import { ServeGifsPage } from '../serve-gifs/serve-gifs';
 import { SearchStickersPage } from '../search-stickers/search-stickers';
@@ -28,7 +25,6 @@ export class HomePage {
   addEmojiPage = AddEmojiPage;
   addTextFacesPage = AddTextFacesPage;
   addCatFacesPage = AddCatFacesPage;
-  addNumbersPage = AddNumbersPage;
   favouritesPage = FavouritesPage;
   coolSymbolsPage = CoolSymbolsPage;
   getGIFSPage = GetGifsPage;
@@ -38,14 +34,12 @@ export class HomePage {
   animalsNatureEmojiPage = AnimalsNaturePage;
   emojiTypesPage = EmojiTypesPage;
   westernTextFacesPage = WesternTextFacesPage;
-  easternTextFacesPage = EasternAnimatedStickersPage;
   serveGIFSPage = ServeGifsPage;
   emojifyPage = EmojifyPage
   public favourite_emoji = [];
   public media_type;
   tab1: any;
   constructor(public navCtrl: NavController, public modalCtrl: ModalController) {
-    this.tab1 = StickerTab;
     this.media_type = "stickers";
   }
 
--- src/pages/trending-stickers/trending-stickers.ts
+++ src/pages/trending-stickers/trending-stickers.ts
@@ -2,16 +2,16 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @Component({
-  selector: 'page-eastern-animated-stickers',
-  templateUrl: 'eastern-animated-stickers.html',
+  selector: 'page-trending-stickers',
+  templateUrl: 'trending-stickers.html',
 })
-export class EasternAnimatedStickersPage {
+export class TrendingStickersPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad EasternAnimatedStickersPage');
+    console.log('ionViewDidLoad TrendingStickersPage');
   }
 
 }
